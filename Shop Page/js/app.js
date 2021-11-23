1499451455aea37a75ca878e4a2205fe2b5afc63
azazmuzaffar <sp18-bcs-035@cuiwah.edu.pk>
2021-11-23 19:25:03 +0500

    Item page desktop version!

--- Shop Page/js/app.js
+++ Shop Page/js/app.js
@@ -1,6 +1,7 @@
 /* >>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<< */
 /* >>>>>>>>>>>>>>>>>>>> Korners <<<<<<<<<<<<<<<<<<< */
 /* >>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<< */
+
 $(".dropdown").click(function () {
   $(this).attr("tabindex", 1).focus();
   $(this).toggleClass("active");
@@ -13,4 +14,21 @@ $(".dropdown").focusout(function () {
 $(".dropdown .dropdown-menu li").click(function () {
   $(this).parents(".dropdown").find("span").text($(this).text());
   $(this).parents(".dropdown").find("input").attr("value", $(this).attr("id"));
+});
+
+var demoTrigger = document.querySelector(".main-image");
+var paneContainer = document.querySelector("._productDetails");
+
+new Drift(demoTrigger, {
+  paneContainer: paneContainer,
+  inlinePane: false,
+});
+
+$(".gallery").slick({
+  infinite: true,
+  slidesToShow: 3,
+  arrows: false,
+  centerPadding: "10px",
+  slidesToScroll: 3,
+  dots: true,
 });
